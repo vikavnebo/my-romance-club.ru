07a4500beaa81201996d3aee08997738aacc326f
Vika <vikavnebo@gmail.com>
2024-01-04 22:44:58 +0000

    Добавить верстку

--- config/settings.py
+++ config/settings.py
@@ -86,13 +86,6 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
-
 DATABASES = {
     'default': {
 	'ENGINE': 'django.db.backends.postgresql',
@@ -139,6 +132,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'main/static',
+    BASE_DIR / 'stories/static',
+
+]
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
