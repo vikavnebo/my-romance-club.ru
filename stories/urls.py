cb17711d8266b4748941777d58e1bf223246b699
Vika <vikavnebo@gmail.com>
2024-01-09 23:29:05 +0000

    Переписать views

--- stories/urls.py
+++ stories/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', StoryView.as_view(), name='stories'),
-    path('<slug:story>/', CharacterView.as_view(), name='story_detail'),
-    path('<slug:story>/<slug:url>/', CharacterDetailView.as_view(), name='character_detail')
+    path('', StoryView.as_view(), name='story_list'),
+    path('<slug:story>/', CharacterView.as_view(), name='character_list'),
+    path('<slug:story>/<slug:character>/', SceneView.as_view(), name='scene_list')
 ]
 
--- stories/views.py
+++ stories/views.py
@@ -5,28 +5,38 @@ from .models import *
 class StoryView(ListView):
 	"""Список историй"""
 	model = Story
-	queryset = Story.objects.all().order_by('-date')
 	context_object_name = 'stories'
+	queryset = Story.objects.all().order_by('-date')
 
 
 class CharacterView(ListView):
-	"""Список персонажей конкретной истории"""
+	"""Список персонажей"""
 	model = Character
 	context_object_name = 'characters'
-	slug_field = 'story'
 
 	def get_queryset(self):
-		queryset = super().get_queryset()
-		story = self.kwargs['story']
-		queryset = queryset.filter(story__url=story)
-		return queryset
+		story_url = self.kwargs['story']
+		queryset = Character.objects.filter(story__url=story_url)
+		return queryset.order_by('name')
+
+	def get_context_data(self, *, object_list=None, **kwargs):
+		context = super().get_context_data(**kwargs)
+		context['story_name'] = Story.objects.get(url=self.kwargs['story']).name
+		return context
 
 
-class CharacterDetailView(DetailView):
-	"""Страница персонажа"""
-	model = Character
-	slug_field = 'url'
-	slug_url_kwarg = 'url'
+class SceneView(ListView):
+	"""Список сцен"""
+	model = Scene
+	context_object_name = 'scenes'
 
 	def get_queryset(self):
-		return super().get_queryset().select_related('story')
+		character_url = self.kwargs['character']
+		queryset = Scene.objects.filter(characters__url=character_url)
+		return queryset.order_by('name')
+
+	def get_context_data(self, *, object_list=None, **kwargs):
+		context = super().get_context_data(**kwargs)
+		context['story_name'] = Story.objects.get(url=self.kwargs['story']).name
+		context['character_name'] = Character.objects.get(url=self.kwargs['character']).name
+		return context
