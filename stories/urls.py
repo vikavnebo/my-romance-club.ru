d2b8ee5fd65e068c9c98bfa604d03ca22b4a658a
Vika <vikavnebo@gmail.com>
2024-01-19 15:02:15 +0000

    Изменить настройки, подключить redis

--- stories/urls.py
+++ stories/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import *
+from django.views.decorators.cache import cache_page
+
 
 urlpatterns = [
-    path('', StoryView.as_view(), name='story_list'),
-    path('<slug:story>/', CharacterView.as_view(), name='character_list'),
-    path('<slug:story>/<slug:character>/', SceneView.as_view(), name='scene_list')
+    path('', cache_page(60 * 15)(StoryView.as_view()), name='story_list'),
+    path('<slug:story>/', cache_page(60 * 15)(CharacterView.as_view()), name='character_list'),
+    path('<slug:story>/<slug:character>/', cache_page(60 * 15)(SceneView.as_view()), name='scene_list')
 ]
 
