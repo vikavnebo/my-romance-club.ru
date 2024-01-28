e52635f40cfebb0d806834c2e318dfc073b23b06
Vika <vikavnebo@gmail.com>
2024-01-28 20:34:04 +0000

    Добавить views ить urls и views для новостей

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from .views import *
+from django.views.decorators.cache import cache_page
 
 
 urlpatterns = [
-	path('news/', get_news_page, name='news'),
+	path('news/', NewView.as_view(), name='new_list'),
+	path('news/<int:pk>/', cache_page(60 * 15)(NewDetail.as_view()), name='new_detail'),
 	path('about/', get_about_page, name='about')
 ]
+
--- main/views.py
+++ main/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponseNotFound
+from django.views.generic import DetailView, ListView
+from .models import *
 
 
-def get_news_page(request):
-	return render(request, 'main/news.html')
+class NewView(ListView):
+	"""Список новостей"""
+	model = New
+	context_object_name = 'news'
+	allow_empty = False
+
+	def get_queryset(self):
+		return New.objects.filter(draft=False)
+
+
+class NewDetail(DetailView):
+	"""Вывод конкретной истории"""
+	model = New
+	context_object_name = 'news'
+
+	def get_queryset(self):
+		return New.objects.filter(pk=self.kwargs['pk'], draft=False)
 
 
 def get_about_page(request):
@@ -14,7 +31,7 @@ def page_not_found(request, exception):
 	return HttpResponseNotFound(
 		'''<h1>
 		Страница не существует. Вы можете:
-		- перейти в <a href="{% url 'stories' %}">каталог историй</a>
-		- прочитать подробнее <a href="{% url 'about' %}">об игре</a>
-		- узнать последние <a href="{% url 'news' %}">новости</a>
+		- перейти в <a href="https://my-romance-club.ru/">каталог историй</a>
+		- прочитать подробнее <a href="https://my-romance-club.ru/about">об игре</a>
+		- узнать последние <a href="https://my-romance-club.ru/news">новости</a>
 		</h1>''')
