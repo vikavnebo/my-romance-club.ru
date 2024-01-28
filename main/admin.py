7ea6a2540dae8fcf0ea1742e9c3e5cea494fd451
Vika <vikavnebo@gmail.com>
2024-01-28 20:05:45 +0000

    Создать модель для новостей и зарегистрировать в админке

--- main/admin.py
+++ main/admin.py
@@ -2,4 +2,14 @@ from django.contrib import admin
 from main.models import *
 
 
+class NewAdmin(admin.ModelAdmin):
+	list_display = ('title', 'date', 'draft', 'image')
+	list_display_links = ('title',)
+	search_fields = ('title',)
+	list_editable = ('draft', 'image',)
+	list_filter = ('date',)
+
+
 admin.site.register(File)
+admin.site.register(New, NewAdmin)
+
--- main/models.py
+++ main/models.py
@@ -1,4 +1,21 @@
 from django.db import models
+from datetime import date
+
+
+class New(models.Model):
+	title = models.CharField('Заголовок', max_length=120)
+	text = models.TextField('Текст')
+	date = models.DateField('Дата публикации', default=date.today)
+	draft = models.BooleanField('Черновик', default=False)
+	image = models.ImageField('Изображение', blank=True, null=True, upload_to='main/news')
+
+	def __str__(self):
+		return str(self.title)
+
+	class Meta:
+		verbose_name = 'Новость'
+		verbose_name_plural = 'Новости'
+		ordering = ['-date']
 
 
 class File(models.Model):
@@ -11,3 +28,4 @@ class File(models.Model):
 	class Meta:
 		verbose_name = 'Файл'
 		verbose_name_plural = 'Файлы'
+
