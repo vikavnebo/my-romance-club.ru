43440630cc6f3a34b19c8b56224400998c5a3150
Vika <129420576+vikavnebo@users.noreply.github.com>
2024-02-01 17:53:59 +0300

    Добавить breadcrumbs для персонажей и сцен

--- stories/views.py
+++ stories/views.py
@@ -22,7 +22,14 @@ class CharacterView(ListView):
 
 	def get_context_data(self, *, object_list=None, **kwargs):
 		context = super().get_context_data(**kwargs)
+
 		context['story_name'] = Story.objects.get(url=self.kwargs['story']).name
+		context['story_url'] = Story.objects.get(url=self.kwargs['story']).url
+
+		context['breadcrumbs'] = (
+			{'name': 'Истории', 'url': '/'},
+			{'name': context['story_name'], 'url': f"/{context['story_url']}"},
+		)
 		return context
 
 
@@ -38,6 +45,13 @@ class SceneView(ListView):
 
 	def get_context_data(self, *, object_list=None, **kwargs):
 		context = super().get_context_data(**kwargs)
+
 		context['story_name'] = Story.objects.get(url=self.kwargs['story']).name
 		context['character_name'] = Character.objects.get(url=self.kwargs['character']).name
+
+		context['breadcrumbs'] = (
+			{'name': 'Истории', 'url': '/'},
+			{'name': context['story_name'], 'url': f"/{self.kwargs['story']}"},
+			{'name': context['character_name'], 'url': f"/{self.kwargs['story']}/{self.kwargs['character']}"},
+		)
 		return context
