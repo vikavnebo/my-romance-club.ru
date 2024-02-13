366fa6f0029ad65b37fdc7d4da67f91d320077ce
Vika <vikavnebo@gmail.com>
2024-02-13 13:00:28 +0000

    Добавить страницу новости обратно

--- main/views.py
+++ main/views.py
@@ -27,21 +27,21 @@ class NewView(ListView):
 		return New.objects.filter(draft=False)
 
 
-# class NewDetail(DetailView):
-# 	"""Вывод конкретной истории"""
-# 	model = New
-# 	context_object_name = 'new'
-#
-# 	def get_queryset(self):
-# 		return New.objects.filter(pk=self.kwargs['pk'], draft=False)
-#
-# 	def get_context_data(self, *, object_list=None, **kwargs):
-# 		context = super().get_context_data(**kwargs)
-#
-# 		context['new_title'] = New.objects.get(pk=self.kwargs['pk'], draft=False).title
-#
-# 		context['breadcrumbs'] = (
-# 			{'name': 'Новости', 'url': f"/news"},
-# 			{'name': context['new_title'], 'url': f"/news/{self.kwargs['pk']}"},
-# 		)
-# 		return context
+class NewDetail(DetailView):
+	"""Вывод конкретной истории"""
+	model = New
+	context_object_name = 'new'
+
+	def get_queryset(self):
+		return New.objects.filter(pk=self.kwargs['pk'], draft=False)
+
+	def get_context_data(self, *, object_list=None, **kwargs):
+		context = super().get_context_data(**kwargs)
+
+		context['new_title'] = New.objects.get(pk=self.kwargs['pk'], draft=False).title
+
+		context['breadcrumbs'] = (
+			{'name': 'Новости', 'url': f"/news"},
+			{'name': context['new_title'], 'url': f"/news/{self.kwargs['pk']}"},
+		)
+		return context
